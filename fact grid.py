fd8c1a35e728d2e9cad35960c2a321d31975fece
Arif khan <arifuol.cs.f.22.123.bs@gmail.com>
2026-04-26 10:35:31 +0500

    Delete fact grid.py

--- fact grid.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-r=pd.read_csv('penguins (1).csv')# FacetGrid is used to plot seprate graph
-f=sns.FacetGrid(r,col="sex")
-f.map(plt.scatter,"island","bill_length_mm").add_legend()
-plt.show()

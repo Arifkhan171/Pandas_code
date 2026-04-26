96cbc2b0a99b60a019ca7841b9fd95c2c2088a00
Arif khan <arifuol.cs.f.22.123.bs@gmail.com>
2026-04-26 10:32:06 +0500

    Delete box plot.py

--- box plot.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-r=pd.read_csv('penguins (1).csv')
-sns.boxplot(x="island",y="flipper_length_mm",data=r,hue="sex",showmeans=True,)
-plt.show()
-
-
-

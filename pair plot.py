0a51e7f1319b21137992e55da81106041b16023c
Arif khan <arifuol.cs.f.22.123.bs@gmail.com>
2026-04-26 10:33:02 +0500

    Delete pair plot.py

--- pair plot.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-r=pd.read_csv('penguins (1).csv').head(150)
-sns.pairplot(r,hue="sex",palette="Dark2",kind="scatter")#scattr, hist,reg,kde
-plt.show()
